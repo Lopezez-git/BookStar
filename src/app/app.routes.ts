0f2305308e2af2057f828ca876f7bbca93ab9cd0
Allan Lopes <lopes.shigame@gmail.com>
2025-10-18 23:09:03 -0300

    Rota da home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,4 +2,6 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './paginas/home/home';
 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent}
+];
