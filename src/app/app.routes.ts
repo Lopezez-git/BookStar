e18b129c25cc85f3def656eea718895577705212
Allan Lopes <lopes.shigame@gmail.com>
2025-10-20 22:53:07 -0300

    Add rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './paginas/home/home';
+import { Cadastro } from './paginas/cadastro/cadastro';
+
 
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent}
+    {path: '', component: HomeComponent},
+    {path: 'cadastro', component: Cadastro}
 ];
