c2c7683d9f9899c825336500d32a7ede6bf26737
Thacris <thalytacris.santana@gmail.com>
2025-10-24 16:47:14 -0300

    home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './paginas/home/home';
 import { Cadastro } from './paginas/cadastro/cadastro';
-
+import { Livros } from './paginas/livros/livros';
+import { Livro} from './paginas/livro/livro';
 
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'cadastro', component: Cadastro}
+    {path: 'cadastro', component: Cadastro},
+     { path: 'livros', component: Livros },
+    { path: 'livro', component: Livro }
 ];
