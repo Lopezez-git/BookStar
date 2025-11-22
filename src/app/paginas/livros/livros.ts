fb09a849d76ee7fa31f9f001a060d3bf4b60870f
Allan Lopes <lopes.shigame@gmail.com>
2025-11-21 23:21:21 -0300

    add mais livros

--- src/app/paginas/livros/livros.ts
+++ src/app/paginas/livros/livros.ts
@@ -13,35 +13,51 @@ import { HeaderComponent } from "../../header/header";
   providers: [LivrosService]
 })
 export class LivrosComponent implements OnInit {
-  livros: any[] = [];
-  carregando = false;
-  erro = '';
+  livrosPopulares: any[] = [];
 
-  constructor(private livrosService: LivrosService, private router: Router) {}
+  constructor() { }
 
-  ngOnInit(): void {
-    this.buscarLivros('suspense');
-  }
+  ngOnInit() {
+    // Apenas exemplo — depois você troca pelas imagens reais
 
-  buscarLivros(termo: string): void {
-    this.carregando = true;
-    this.livrosService.buscarLivros(termo, 40).subscribe({
-      next: (res) => {
-        this.livros = res.items || [];
-        this.carregando = false;
-      },
-      error: () => {
-        this.erro = 'Erro ao buscar livros.';
-        this.carregando = false;
-      }
-    });
-  }
+    this.livrosPopulares = [
+      { img: '/livros/livro1.png' },
+      { img: '/livros/livro2.png' },
+      { img: '/livros/livro3.png' },
+      { img: '/livros/livro4.png' },
+      { img: '/livros/livro5.png' },
+      { img: '/livros/livro6.png' },
+      { img: '/livros/livro7.png' },
+      { img: '/livros/livro8.png' },
+      { img: '/livros/livro9.png' },
+      { img: '/livros/livro10.png' },
+      { img: '/livros/livro11.png' },
+      { img: '/livros/livro12.png' },
+      { img: '/livros/livro13.png' },
+      { img: '/livros/livro14.png' },
+      { img: '/livros/livro15.png' },
+      { img: '/livros/livro16.png' },
+      { img: '/livros/livro17.png' },
+      { img: '/livros/livro18.png' },
+      { img: '/livros/livro19.png' },
+      { img: '/livros/livro20.png' },
+      { img: '/livros/livro21.png' },
+      { img: '/livros/livro22.png' },
+      { img: '/livros/livro23.png' },
+      { img: '/livros/livro24.png' },
+      { img: '/livros/livro25.png' },
+      { img: '/livros/livro26.png' },
+      { img: '/livros/livro27.png' },
+      { img: '/livros/livro29.jpg' },
+      { img: '/livros/livro30.jpg' },
+      { img: '/livros/livro31.jpg' },
+      { img: '/livros/livro32.jpg' },
+      { img: '/livros/livro33.jpg' },
+      { img: '/livros/livro34.jpg' },
+      { img: '/livros/livro35.jpg' },
+      { img: '/livros/livro36.jpg' },
 
-  getImagemLivro(livro: any): string {
-    return livro.volumeInfo?.imageLinks?.thumbnail || 'assets/placeholder-book.jpg';
-  }
 
-  abrirLivro(livro: any) {
-    this.router.navigate(['/livro', livro.id]);
+    ];
   }
 }
